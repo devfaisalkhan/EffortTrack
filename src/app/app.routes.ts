f79b7f0515995174d34dbbd0222fc6ab357c6a00
Faisal khan <dev.faisalkhan@gmail.com>
2025-08-10 22:04:49 +0500

    basic design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TimerComponent } from './timer/timer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TimerComponent }
+];
---
+++ src/app/services/timer.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, interval, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface TimeLog {
+  start: Date;
+  end: Date;
+  duration: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+  private timerRunning = false;
+  private startTime: Date | null = null;
+  private timerSubscription: any;
+
+  private timeSubject = new BehaviorSubject<string>('00:00:00');
+  private logsSubject = new BehaviorSubject<TimeLog[]>([]);
+
+  time$ = this.timeSubject.asObservable();
+  logs$ = this.logsSubject.asObservable();
+
+  constructor() {
+    this.loadLogs();
+  }
+
+  startTimer() {
+    if (!this.timerRunning) {
+      this.timerRunning = true;
+      this.startTime = new Date();
+      this.timerSubscription = interval(1000).pipe(
+        map(() => this.formatDuration(new Date().getTime() - this.startTime!.getTime()))
+      ).subscribe(time => this.timeSubject.next(time));
+    }
+  }
+
+  stopTimer() {
+    if (this.timerRunning && this.startTime) {
+      this.timerRunning = false;
+      const endTime = new Date();
+      const duration = this.formatDuration(endTime.getTime() - this.startTime.getTime());
+      
+      const log: TimeLog = {
+        start: this.startTime,
+        end: endTime,
+        duration
+      };
+
+      const currentLogs = this.logsSubject.value;
+      const updatedLogs = [log, ...currentLogs];
+      
+      localStorage.setItem('timeLogs', JSON.stringify(updatedLogs));
+      this.logsSubject.next(updatedLogs);
+      
+      if (this.timerSubscription) {
+        this.timerSubscription.unsubscribe();
+      }
+      
+      this.timeSubject.next('00:00:00');
+      this.startTime = null;
+    }
+  }
+
+  private loadLogs() {
+    const savedLogs = localStorage.getItem('timeLogs');
+    if (savedLogs) {
+      const logs: TimeLog[] = JSON.parse(savedLogs);
+      // Convert string dates back to Date objects
+      const parsedLogs = logs.map(log => ({
+        ...log,
+        start: new Date(log.start),
+        end: new Date(log.end)
+      }));
+      this.logsSubject.next(parsedLogs);
+    }
+  }
+
+  private formatDuration(ms: number): string {
+    const seconds = Math.floor((ms / 1000) % 60);
+    const minutes = Math.floor((ms / (1000 * 60)) % 60);
+    const hours = Math.floor(ms / (1000 * 60 * 60));
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
+}
